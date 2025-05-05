97fd412b3a2f228c12336c6da3629022dc9aae88
alimohamedelnaggar <99051442+alimohamedelnaggar@users.noreply.github.com>
2025-05-05 18:29:11 +0300

    update

---
+++ Talabat.Repository/Data/Contexts/TalabatDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+
+namespace Talabat.Repository.Data.Contexts
+{
+    class TalabatDbContext:DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        DbSet<Product> Products { get; set; }
+        DbSet<ProductBrand> ProductBrands { get; set; }
+        DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
